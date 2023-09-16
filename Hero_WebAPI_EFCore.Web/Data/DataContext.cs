4f01dad46623996c50301c20c10eb4b2c258f2c7
Paulo de Tarso <potassio@hotmail.com>
2023-09-16 18:57:15 -0300

    Criação da classe contexto

---
+++ Hero_WebAPI_EFCore.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Hero_WebAPI_EFCore.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hero_WebAPI_EFCore.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Secret> Secrets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=localhost;DataBase=Hero_WebAPI_EFCore_DB;Uid=dev;Pwd=1234567");
+        }
+    }
+}
