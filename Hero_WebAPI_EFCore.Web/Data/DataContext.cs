aebf62789e782df81300fa947bf1081235eb8c9a
Paulo de Tarso <potassio@hotmail.com>
2023-09-16 19:41:13 -0300

    Ajuste nas relações N:N na classe DataContext

--- Hero_WebAPI_EFCore.Web/Data/DataContext.cs
+++ Hero_WebAPI_EFCore.Web/Data/DataContext.cs
@@ -5,14 +5,27 @@ namespace Hero_WebAPI_EFCore.Web.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<Secret> Secrets { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
         public DbSet<Hero> Heroes { get; set; }
         public DbSet<Movie> Movies { get; set; }
-        public DbSet<Weapon> Weapons { get; set; }
-        public DbSet<Secret> Secrets { get; set; }
+        public DbSet<HeroMovie> HeroesMovies { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySQL("Server=localhost;DataBase=Hero_WebAPI_EFCore_DB;Uid=developer;Pwd=1234567");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroMovie>(entity =>
+            {
+                entity.HasKey(e => new
+                {
+                    e.HeroId,
+                    e.MovieId
+                });
+            });
         }
     }
 }
