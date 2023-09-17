186ea15913a05a343d3d4c3997b2039dc361cda9
Paulo de Tarso <potassio@hotmail.com>
2023-09-17 17:04:41 -0300

    Adição do insert no serviço de filme

--- Hero_WebAPI_EFCore.Web/Services/MovieService.cs
+++ Hero_WebAPI_EFCore.Web/Services/MovieService.cs
@@ -1,4 +1,5 @@
-﻿using Hero_WebAPI_EFCore.DAL.Repositories.Interfaces;
+﻿using Hero_WebAPI_EFCore.DAL.Repositories;
+using Hero_WebAPI_EFCore.DAL.Repositories.Interfaces;
 using Hero_WebAPI_EFCore.Domain.Models;
 using Hero_WebAPI_EFCore.Web.Models;
 using Hero_WebAPI_EFCore.Web.Services.Interfaces;
@@ -67,12 +68,45 @@ namespace Hero_WebAPI_EFCore.Web.Services
 
         public MovieViewModel GetByName(string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Movie entity = _movieRepository.GetByName(name);
+
+                if (entity is null)
+                    return null;
+
+                return new MovieViewModel
+                {
+                    MovieId = entity.MovieId,
+                    Name = entity.Name,
+                    Rate = entity.Rate
+                };
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public bool Insert(MovieViewModel model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (_movieRepository.GetByName(model.Name) is not null)
+                    throw new Exception("Nome já consta na base de dados.");
+
+                Movie entity = new()
+                {
+                    Name = model.Name,
+                    Rate = model.Rate
+                };
+
+                return _movieRepository.Insert(entity);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public bool Update(MovieViewModel model)
