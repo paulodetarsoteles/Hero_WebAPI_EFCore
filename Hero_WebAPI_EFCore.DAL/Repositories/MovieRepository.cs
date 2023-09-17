1fe4e0a63d0ca3c8257830b7ea0e222f85f4e1ee
Paulo de Tarso <potassio@hotmail.com>
2023-09-17 16:30:54 -0300

    Implantação de Get, GetById, GetByName e Insert no repositório de filmes

--- Hero_WebAPI_EFCore.DAL/Repositories/MovieRepository.cs
+++ Hero_WebAPI_EFCore.DAL/Repositories/MovieRepository.cs
@@ -15,22 +15,84 @@ namespace Hero_WebAPI_EFCore.DAL.Repositories
 
         public List<Movie> Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dataContext.Movies.ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception($"Erro no banco de dados.");
+            }
         }
 
-        public Movie GetById(int id)
+        public Movie? GetById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Movie movie;
+
+                try
+                {
+                    movie = _dataContext.Movies.First(h => h.MovieId == id);
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+
+                return movie;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception($"Erro no banco de dados.");
+            }
         }
 
         public Movie? GetByName(string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Movie movie;
+
+                try
+                {
+                    movie = _dataContext.Movies.First(h => h.Name == name);
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+
+                return movie;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception($"Erro no banco de dados.");
+            }
         }
 
         public bool Insert(Movie entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dataContext.Movies.Add(entity);
+                int entitiesSaved = _dataContext.SaveChanges();
+
+                if (entitiesSaved <= 0)
+                    return false;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception($"Erro no banco de dados.");
+            }
         }
 
         public bool Update(Movie entity)
