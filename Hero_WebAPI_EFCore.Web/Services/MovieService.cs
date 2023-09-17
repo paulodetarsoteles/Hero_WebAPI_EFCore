c7c4f3fd4b29227cecc87c68fac6efb067848933
Paulo de Tarso <potassio@hotmail.com>
2023-09-17 16:58:12 -0300

    Implantação no serviço de filmes do Get e GetById

--- Hero_WebAPI_EFCore.Web/Services/MovieService.cs
+++ Hero_WebAPI_EFCore.Web/Services/MovieService.cs
@@ -1,4 +1,6 @@
-﻿using Hero_WebAPI_EFCore.DAL.Repositories.Interfaces;
+﻿using Hero_WebAPI_EFCore.DAL.Repositories;
+using Hero_WebAPI_EFCore.DAL.Repositories.Interfaces;
+using Hero_WebAPI_EFCore.Domain.Models;
 using Hero_WebAPI_EFCore.Web.Models;
 using Hero_WebAPI_EFCore.Web.Services.Interfaces;
 
@@ -15,12 +17,53 @@ namespace Hero_WebAPI_EFCore.Web.Services
 
         public List<MovieViewModel> Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<Movie> entities = _movieRepository.Get();
+
+                if (entities is null || entities.Count == 0)
+                    return null;
+
+                List<MovieViewModel> models = new();
+
+                foreach (Movie movie in entities)
+                {
+                    models.Add(new MovieViewModel
+                    {
+                        MovieId = movie.MovieId,
+                        Name = movie.Name,
+                        Rate = movie.Rate
+                    });
+                }
+
+                return models;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public MovieViewModel GetById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Movie entity = _movieRepository.GetById(id);
+
+                if (entity is null) 
+                    return null;
+
+                return new MovieViewModel
+                {
+                    MovieId = entity.MovieId,
+                    Name = entity.Name,
+                    Rate = entity.Rate
+                };
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
 
         public bool Insert(MovieViewModel model)
