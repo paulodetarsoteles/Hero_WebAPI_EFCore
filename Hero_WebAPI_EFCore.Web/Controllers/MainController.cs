eeedd6941485592e7233c0bce5d6eff9a5001606
Paulo de Tarso <potassio@hotmail.com>
2023-09-17 10:41:37 -0300

    Adição dos middleware na program e primeira controller

--- Hero_WebAPI_EFCore.Web/Controllers/MainController.cs
+++ Hero_WebAPI_EFCore.Web/Controllers/MainController.cs
@@ -1,13 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hero_WebAPI_EFCore.Web.Models;
+using Hero_WebAPI_EFCore.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 
 namespace Hero_WebAPI_EFCore.Web.Controllers
 {
     [Route("api/[controller]")]
     public class MainController : Controller
     {
+        private readonly IHeroService _heroService;
+        private readonly IMovieService _movieService;
+        private readonly ISecretService _secretService;
+        private readonly IWeaponService _weaponService;
+
+        public MainController(IHeroService heroService, IMovieService movieService, ISecretService secretService, IWeaponService weaponService)
+        {
+            _heroService = heroService;
+            _movieService = movieService;
+            _secretService = secretService;
+            _weaponService = weaponService;
+        }
+
         public IActionResult Index()
         {
             return Ok("API Running...");
         }
+
+        #region Heroes
+
+        [HttpPost]
+        public IActionResult CreateHero(HeroViewModel model)
+        {
+            try
+            {
+                if (model is null)
+                    return BadRequest("Modelo enviado está nulo.");
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                bool result = _heroService.Insert(model);
+
+                if (!result)
+                    throw new Exception("Erro ao salvar modelo."); 
+
+                return CreatedAtAction(MethodBase.GetCurrentMethod().ToString(), $"Id: {model.HeroId}", model);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        #endregion
+
+        #region Movies
+        #endregion
+
+        #region Secrets
+        #endregion
+
+        #region Weapons
+        #endregion
     }
 }
--- Hero_WebAPI_EFCore.Web/Program.cs
+++ Hero_WebAPI_EFCore.Web/Program.cs
@@ -1,4 +1,8 @@
 using Hero_WebAPI_EFCore.DAL.Data;
+using Hero_WebAPI_EFCore.DAL.Repositories.Interfaces;
+using Hero_WebAPI_EFCore.DAL.Repositories;
+using Hero_WebAPI_EFCore.Web.Services.Interfaces;
+using Hero_WebAPI_EFCore.Web.Services;
 using Microsoft.EntityFrameworkCore;
 
 internal class Program
@@ -8,6 +12,16 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+
+        builder.Services.AddScoped<IHeroRepository, HeroRepository>();
+        builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+        builder.Services.AddScoped<ISecretRepository, SecretRepository>();
+        builder.Services.AddScoped<IWeaponRepository, WeaponRepository>();
+
+        builder.Services.AddScoped<IHeroService, HeroService>();
+        builder.Services.AddScoped<IMovieService, MovieService>();
+        builder.Services.AddScoped<ISecretService, SecretService>();
+        builder.Services.AddScoped<IWeaponService, WeaponService>();
 
         builder.Services.AddDbContext<DataContext>(options =>
             {
