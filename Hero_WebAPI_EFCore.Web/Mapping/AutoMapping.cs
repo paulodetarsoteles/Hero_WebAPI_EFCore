87e89c33ea7f9135fdc8763d460ce2c7954b79f6
Paulo de Tarso <potassio@hotmail.com>
2023-09-22 19:42:05 -0300

    Criação da classe AutoMapping e seu construtor

---
+++ Hero_WebAPI_EFCore.Web/Mapping/AutoMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Hero_WebAPI_EFCore.Domain.Models;
+using Hero_WebAPI_EFCore.Web.Models;
+
+namespace Hero_WebAPI_EFCore.Web.Mapping
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<Movie, MovieViewModel>();
+            CreateMap<MovieViewModel, Movie>();
+            CreateMap<Hero, HeroViewModel>();
+            CreateMap<HeroViewModel, Hero>();//.ForMember(x => x.Weapons, opt => opt.Ignore());
+            CreateMap<Weapon, WeaponViewModel>();
+            CreateMap<WeaponViewModel, Weapon>();
+            CreateMap<Secret, SecretViewModel>();
+            CreateMap<SecretViewModel, Secret>();
+        }
+    }
+}
